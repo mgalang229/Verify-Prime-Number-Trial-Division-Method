1d6106adc05312a8085bd7d28e42a573859280f7
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-05-23 10:36:07 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool prime = true;
+
+bool checker(int n){
+	for(int i = 2; i <= n/2; i++){
+		if(n%i == 0){
+			prime = false;
+			break;
+		}
+	}
+	return prime;
+}
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int x;
+	cin >> x;
+	if(x == 0 || x == 1){
+		cout << x << " -> neither prime nor composite";
+	} else if(checker(x)){
+		cout << x << " -> prime";
+	} else{
+		cout << x << " -> composite";
+	}
+	cout << "\n";
+	return 0;
+}
